22b2b46f3554b5ee5b3f53084ec59069fba9c537
bmulaw <67084560+bmulaw@users.noreply.github.com>
2021-08-04 23:29:38 -0400

    got counter an added to list

--- js/example.js
+++ js/example.js
@@ -1,10 +1,16 @@
 // ADD NEW ITEM TO END OF LIST
-
+var page = document.getElementById('page');
+page.innerHTML += "<li class='cool' id='six'> cream </li>";
 
 // ADD NEW ITEM START OF LIST
-
+page.innerHTML = "<li class='cool' id='five'> kale </li>" + page.innerHTML;
 
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
+// https://www.w3schools.com/jsref/prop_html_classname.asp
+page.className = "cool";
 
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+var h2 = document.getElementsByTagName('h2');
+var allList = document.getElementsByTagName('li');
+h2[0].innerHTML += " " + allList.length;
