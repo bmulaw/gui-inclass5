a3e25a43ea4894a4c45cd775c4042810ac58d92e
bmulaw <67084560+bmulaw@users.noreply.github.com>
2021-08-04 23:38:27 -0400

    got colors right

--- js/example.js
+++ js/example.js
@@ -9,8 +9,10 @@ page.innerHTML = "<li class='cool' id='five'> kale </li>" + page.innerHTML;
 // https://www.w3schools.com/jsref/prop_html_classname.asp
 page.className = "cool";
 
-
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
 var h2 = document.getElementsByTagName('h2');
 var allList = document.getElementsByTagName('li');
+for (var li of allList){
+    li.className = "cool";
+}
 h2[0].innerHTML += " " + allList.length;
